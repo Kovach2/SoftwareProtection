976d2cb08ca62e9bb03db90ac92ff8da145e5241
Egor <egorka-kovalenko92@mail.ru>
2022-10-26 18:00:05 +0300

    test mobile

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Home</title>
+            </Head>
+            <main className={styles.home}>
+                <h1 className={styles.title}>Software protection</h1>
+                <div className={styles.buttons}>
+                    <div className={styles.buttons_line}>
+                        <Link href={"lr"} className={styles.button_lr}>
+                            Лабораторные работы
+                        </Link>
+                        <Link href={"#"} className={styles.button}>
+                            Документация
+                        </Link>
+                    </div>
+                    <div className={styles.buttons_lineUnder}>
+                        <Link href={"#"} className={styles.button}>
+                            Разработчики
+                        </Link>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
