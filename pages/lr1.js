c9b6e96a3a152fface6c5d299f1dd7066ba839ed
Egor <egorka-kovalenko92@mail.ru>
2022-11-16 00:08:24 +0300

    problemsssss

--- pages/lr1.js
+++ pages/lr1.js
@@ -11,132 +11,136 @@ import rightBiceps from "../public/mainPageLabs/rightBiceps.png";
 
 import classNames from "classnames";
 
+if(typeof window === "undefined") {
+
+
 // Задание 1
 
-let symbols = ""
+    let symbols = ""
 
-function PTV(){
-    let arr = document.querySelectorAll(".input");
-    let p = arr[0].querySelector("input").value;
-    let v = arr[1].querySelector("input").value;
-    let t = arr[2].querySelector("input").value;
-    if(isNaN(v*t/p) || Number.isInteger(p) || Number.isInteger(v) || Number.isInteger(t) || p == "" || v =="" || t ==""){
-        alert("Проверьте данные при вводе PVT")
-    }else{
-        arr[3].querySelector("input").value = (v*t/p).toFixed(3)
-    }
-}
-
-function formPassSymbols(){
-    let checkboxs = document.querySelectorAll(".checkbox")
-    let symbolsArr = ["абвгдеёжзийклмнопрстуфхцчшщъыьэюя","АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ","abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ","0123456789",'!"#$%&()*+,-./:;<=>?@[\]^_`{|}~']
-    symbols = ""
-    if(checkboxs[0].checked){
-        symbols += symbolsArr[0]
-    }
-    if(checkboxs[1].checked){
-        symbols += symbolsArr[1]
-    }
-    if(checkboxs[2].checked){
-        symbols += symbolsArr[2]
-    }
-    if(checkboxs[3].checked){
-        symbols += symbolsArr[3]
-    }
-    if(checkboxs[4].checked){
-        symbols += symbolsArr[4]
-    }
-    if(checkboxs[5].checked){
-        symbols += symbolsArr[5]
-    }
-}
-
-function generateRandomPass(){
-    let passwordLine = document.querySelector(".password_line")
-    let arr = document.querySelectorAll(".input");
-    let pass = ""
-    let length = Number(arr[4].querySelector("input").value);
-
-    if(Number.isInteger(length) && length != 0 && 6 <= length && length <= 18 && symbols.length != 0)
-    {
-        for (let i = 0; i < length; i++) {
-            pass += symbols[Math.floor(Math.random() * symbols.length)]
+    function PTV() {
+        let arr = document.querySelectorAll(".input");
+        let p = arr[0].querySelector("input").value;
+        let v = arr[1].querySelector("input").value;
+        let t = arr[2].querySelector("input").value;
+        if (isNaN(v * t / p) || Number.isInteger(p) || Number.isInteger(v) || Number.isInteger(t) || p == "" || v == "" || t == "") {
+            alert("Проверьте данные при вводе PVT")
+        } else {
+            arr[3].querySelector("input").value = (v * t / p).toFixed(3)
         }
-
-    }else{
-        alert("Ошибка")
     }
-    passwordLine.querySelector("input").value = pass
-}
 
-function copyPassword(){
-    let passwordLine = document.querySelector(".password_line")
-    let copyText = passwordLine.querySelector("input")
-    if (copyText != 0){
-        copyText.select()
-        document.execCommand("copy")
+    function formPassSymbols() {
+        let checkboxs = document.querySelectorAll(".checkbox")
+        let symbolsArr = ["абвгдеёжзийклмнопрстуфхцчшщъыьэюя", "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ", "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~']
+        symbols = ""
+        if (checkboxs[0].checked) {
+            symbols += symbolsArr[0]
+        }
+        if (checkboxs[1].checked) {
+            symbols += symbolsArr[1]
+        }
+        if (checkboxs[2].checked) {
+            symbols += symbolsArr[2]
+        }
+        if (checkboxs[3].checked) {
+            symbols += symbolsArr[3]
+        }
+        if (checkboxs[4].checked) {
+            symbols += symbolsArr[4]
+        }
+        if (checkboxs[5].checked) {
+            symbols += symbolsArr[5]
+        }
     }
-}
+
+    function generateRandomPass() {
+        let passwordLine = document.querySelector(".password_line")
+        let arr = document.querySelectorAll(".input");
+        let pass = ""
+        let length = Number(arr[4].querySelector("input").value);
+
+        if (Number.isInteger(length) && length != 0 && 6 <= length && length <= 18 && symbols.length != 0) {
+            for (let i = 0; i < length; i++) {
+                pass += symbols[Math.floor(Math.random() * symbols.length)]
+            }
+
+        } else {
+            alert("Ошибка")
+        }
+        passwordLine.querySelector("input").value = pass
+    }
+
+    function copyPassword() {
+        let passwordLine = document.querySelector(".password_line")
+        let copyText = passwordLine.querySelector("input")
+        if (copyText != 0) {
+            copyText.select()
+            document.execCommand("copy")
+        }
+    }
 
 // Задание 2
 
-let button = document.querySelector("#generatePassword")
-let buttonCopy = document.getElementById("generatePasswordCopy")
-let conditionDescription = document.querySelector(".ex_condition--description")
-let select = document.querySelector(".choice_variat")
-let lineGenerate = document.querySelector(".ex_complete").querySelector("input")
-
-select.addEventListener("change", function(){
-    if(select.value == 11){
-        conditionDescription.innerHTML = "1. b1,b2...b1+q  - случайные символы из множества {!,”,#,$,%,&,’,(,),*}, где  Q=Nmod5. <br>2. Оставшиеся символы пароля, кроме b9 , - случайные малые буквы русские алфавита. <br>3. b9   - случайная цифра."
-    }else{
-        conditionDescription.innerHTML = "1.   - случайные заглавные буквы английского алфавита.<br>2.b3 = N^2 mod 10  (где mod 10 – остаток от деления числа на 10).<br>3.b4 - случайная цифра.<br>4.b5 - случайный символ из множества {!,”,#,$,%,&,’,(,),*}.<br>5.b6 - случайная малая буква русского алфавита."
-    }
-    lineGenerate.value = ""
-})
+    let button = document.querySelector("#generatePassword")
+    let buttonCopy = document.getElementById("generatePasswordCopy")
+    let conditionDescription = document.querySelector(".ex_condition--description")
+    let select = document.querySelector(".choice_variat")
+    let lineGenerate = document.querySelector(".ex_complete").querySelector("input")
 
 
-button.addEventListener("click", function(){
-    let symbolsArr = ["абвгдеёжзийклмнопрстуфхцчшщъыьэюя","ABCDEFGHIJKLMNOPQRSTUVWXYZ","0123456789","!”#$%&’()*"]
-    let len = [9,6]
-    let pass = ""
-    if(select.value == 11){
-        for (let i = 0; i < len[0]; i++) {
-            if (i <= len[0]%5) {
-                pass += symbolsArr[3][Math.floor(Math.random() * symbolsArr[3].length)]
-            }else if(i == 8){
-                pass += symbolsArr[2][Math.floor(Math.random() * symbolsArr[2].length)]
-            }else{
-                pass += symbolsArr[0][Math.floor(Math.random() * symbolsArr[0].length)]
-            }
+
+    select.addEventListener("change", function () {
+        if (select.value == 11) {
+            conditionDescription.innerHTML = "1. b1,b2...b1+q  - случайные символы из множества {!,”,#,$,%,&,’,(,),*}, где  Q=Nmod5. <br>2. Оставшиеся символы пароля, кроме b9 , - случайные малые буквы русские алфавита. <br>3. b9   - случайная цифра."
+        } else {
+            conditionDescription.innerHTML = "1.   - случайные заглавные буквы английского алфавита.<br>2.b3 = N^2 mod 10  (где mod 10 – остаток от деления числа на 10).<br>3.b4 - случайная цифра.<br>4.b5 - случайный символ из множества {!,”,#,$,%,&,’,(,),*}.<br>5.b6 - случайная малая буква русского алфавита."
         }
-        lineGenerate.value = pass
-    }else{
-        for (let i = 0; i < len[1]; i++) {
-            if (i <= 1){
-                pass += symbolsArr[1][Math.floor(Math.random() * symbolsArr[1].length)]
-            }else if(i == 2){
-                pass += (len[1]**2)%10
+        lineGenerate.value = ""
+    })
 
-            }else if(i == 3){
-                pass += symbolsArr[2][Math.floor(Math.random() * symbolsArr[2].length)]
-            }else if(i == 4){
-                pass += symbolsArr[3][Math.floor(Math.random() * symbolsArr[3].length)]
-            }else{
-                pass += symbolsArr[2][Math.floor(Math.random() * symbolsArr[2].length)]
+
+    button.addEventListener("click", function () {
+        let symbolsArr = ["абвгдеёжзийклмнопрстуфхцчшщъыьэюя", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "!”#$%&’()*"]
+        let len = [9, 6]
+        let pass = ""
+        if (select.value == 11) {
+            for (let i = 0; i < len[0]; i++) {
+                if (i <= len[0] % 5) {
+                    pass += symbolsArr[3][Math.floor(Math.random() * symbolsArr[3].length)]
+                } else if (i == 8) {
+                    pass += symbolsArr[2][Math.floor(Math.random() * symbolsArr[2].length)]
+                } else {
+                    pass += symbolsArr[0][Math.floor(Math.random() * symbolsArr[0].length)]
+                }
             }
+            lineGenerate.value = pass
+        } else {
+            for (let i = 0; i < len[1]; i++) {
+                if (i <= 1) {
+                    pass += symbolsArr[1][Math.floor(Math.random() * symbolsArr[1].length)]
+                } else if (i == 2) {
+                    pass += (len[1] ** 2) % 10
+
+                } else if (i == 3) {
+                    pass += symbolsArr[2][Math.floor(Math.random() * symbolsArr[2].length)]
+                } else if (i == 4) {
+                    pass += symbolsArr[3][Math.floor(Math.random() * symbolsArr[3].length)]
+                } else {
+                    pass += symbolsArr[2][Math.floor(Math.random() * symbolsArr[2].length)]
+                }
+            }
+            lineGenerate.value = pass
         }
-        lineGenerate.value = pass
-    }
-})
+    })
 
-buttonCopy.addEventListener("click", function(){
-    if (lineGenerate != 0){
-        lineGenerate.select()
-        document.execCommand("copy")
-    }
-})
-
+    buttonCopy.addEventListener("click", function () {
+        if (lineGenerate != 0) {
+            lineGenerate.select()
+            document.execCommand("copy")
+        }
+    })
+}
 export default function LR1() {
     return (
         <div className={styles.wrapper}>
